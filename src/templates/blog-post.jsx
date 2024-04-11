aa02e77d0fd36f6211bda596a627d9bafb2ed233
Phạm Xuân Thắng <pxthang97@gmail.com>
2024-04-11 17:17:17 +0700

    animation scalling

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -11,17 +11,14 @@ const classes = {
   wrapper: 'mt-16 blog-content',
   title: 'mt-16 text-4xl text-gray-900 font-bold',
   date: 'text-gray-600 font-light',
-  blogPost: {
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
+  blogPost: 'bg-cover bg-center bg-no-repeat h-full w-full flex flex-col justify-center items-center'
 };
 
 const pageTransition = {
-  hidden: { opacity: 0 },
+  hidden: { opacity: 0, scale: 0.8 },
   show: {
     opacity: 1,
+    scale: 1,
     transition: {
       duration: 0.5,
       ease: 'easeIn'
@@ -31,9 +28,14 @@ const pageTransition = {
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
-  console.log(post.frontmatter)
   return (
-    <div className={{ ...classes.blogPost}} style={{ backgroundImage: `url(${post.frontmatter.background})` }}>
+    <motion.div 
+      className={classes.blogPost} 
+      style={{ backgroundImage: `url(${post.frontmatter.background})` }}
+      initial="hidden" 
+      animate="show" 
+      variants={pageTransition}
+    >
      <Layout>
         <Header metadata={data.site.siteMetadata} />
         <SEO title={post.frontmatter.title} />
@@ -61,8 +63,7 @@ const BlogPost = ({ data }) => {
           variants={pageTransition}
         />
       </Layout>
-    </div>
-
+    </motion.div>
   );
 };
 
