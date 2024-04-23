0290916f0a399693bd26a00fa0e32e5f21df7079
praslar <pxthang97@gmail.com>
2024-04-24 00:23:25 +0700

    workflows

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 DOCKER_IMAGE=porfolio:0.0.1
 
 web_build:
-	BUILD_VERSION=$(BUILD_VERSION) docker-compose up -d --build
+	BUILD_VERSION=$(BUILD_VERSION) docker compose up -d --build
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,37 +13,49 @@ module.exports = {
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/pxtha/`,
     // Content of the About Me section
-    about: `Hello! I'm a Software Engineer with nearly 4 years of professional experience under my belt. I have a deep passion for software development and harbor aspirations of establishing my own product in the future. I'm always ready to take on new challenges and grow both personally and professionally.`,
+    about: `Hello! I'm a Software Engineer with nearly 4 years of experience. I have a deep passion for software development and harbor aspirations of establishing my own product in the future. I'm always ready to take on new challenges and grow both personally and professionally.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
         name: 'Joon',
-        description:'Our mission is to connect single Muslims looking for a fulfilling Marriage',
+        description:
+          'Our mission is to connect single Muslims looking for a fulfilling Marriage',
         link: '/blog/joon',
-        images: ['https://i.myjoon.app/0x0,sc,s/joon-media/bca0d162-6fc3-4a23-8939-64c5906c80dc/image/91275444-3167-40fd-84e4-337d4e7b4b27.jpg'],
-        color: '#DEECF2'
+        images: [
+          'https://i.myjoon.app/0x0,sc,s/joon-media/bca0d162-6fc3-4a23-8939-64c5906c80dc/image/91275444-3167-40fd-84e4-337d4e7b4b27.jpg',
+        ],
+        color: '#DEECF2',
       },
       {
         name: 'Wesaidspacesaid',
-        description: 'Create landing page, booking page, and admin page for a coffee station in Vietnam. ',
+        description:
+          'Create landing page, booking page, and admin page for a coffee station in Vietnam. ',
         link: '/blog/wsss',
-        images: ['https://nick.computer/static/media/realas-lg.d9bab4ac14f036998f99.jpg'],
-        color: 'rgb(70, 91, 155)'
+        images: [
+          'https://nick.computer/static/media/realas-lg.d9bab4ac14f036998f99.jpg',
+        ],
+        color: 'rgb(70, 91, 155)',
       },
       {
         name: 'Magic Resume',
-        description:'A resume builder that helps you create and manage professional resumes in minutes.',
+        description:
+          'A resume builder that helps you create and manage professional resumes in minutes.',
         link: '/blog/magic-resume',
-        images: ['https://nick.computer/static/media/fenton-lg.aae58db69fffa6b8d1de.jpg'],
-        color: 'rgb(244, 245, 146)'
+        images: [
+          'https://nick.computer/static/media/fenton-lg.aae58db69fffa6b8d1de.jpg',
+        ],
+        color: 'rgb(244, 245, 146)',
       },
       {
         name: 'Style Simple',
-        description: 'The platform offers a seamless shopping experience across web and mobile applications, built using the headless CMS. ',
+        description:
+          'The platform offers a seamless shopping experience across web and mobile applications, built using the headless CMS. ',
         link: '/blog/style-simple',
-        images: ['https://nick.computer/static/media/br-lg.0231d6c2ec5ff69678d4.jpg'],
-        color: 'rgb(10, 108, 188)'
-      }
+        images: [
+          'https://nick.computer/static/media/br-lg.0231d6c2ec5ff69678d4.jpg',
+        ],
+        color: 'rgb(10, 108, 188)',
+      },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
@@ -76,15 +88,18 @@ module.exports = {
     skills: [
       {
         name: 'Senior Developer',
-        description: 'Experienced senior developer with proficiency in multiple tech stacks. In-depth knowledge of modern application development technologies.',
+        description:
+          'Experienced senior developer with proficiency in multiple tech stacks. In-depth knowledge of modern application development technologies.',
       },
       {
         name: 'Leadership',
-        description: 'Proven ability to lead teams and manage projects effectively.',
+        description:
+          'Proven ability to lead teams and manage projects effectively.',
       },
       {
         name: 'Problem Solving',
-        description: 'Strong problem-solving skills with a knack for finding creative solutions to complex technical issues.',
+        description:
+          'Strong problem-solving skills with a knack for finding creative solutions to complex technical issues.',
       },
     ],
   },
